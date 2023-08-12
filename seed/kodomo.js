14d153128f0261057c9a92dc81342341009b5529
dcritoballl <d.cristobalgallego@gmail.com>
2023-08-12 14:26:19 +0200

    database uploaded

---
+++ seed/kodomo.js
@@ -0,0 +1,52 @@
+const kodomo=[
+    {
+        title:"Doraemon Color nº 01",
+        numVolume: 1,
+        collection:"Doraemon",
+        description:"Tomo manga kodomo de Doraemon, con elque se podrá disfrutar del personaje en su color original. Una colección que, además, recoge historias no publicadas en la edición más conocida del personaje.",
+        genre:"kodomo",
+        image:"https://m.media-amazon.com/images/I/51NYhOsPSSL._SX365_BO1,204,203,200_.jpg",
+    },
+    {
+        title:"Doraemon Color nº 02",
+        numVolume: 2,
+        collection:"Doraemon",
+        description:"Tomo manga kodomo de Doraemon, con elque se podrá disfrutar del personaje en su color original. Una colección que, además, recoge historias no publicadas en la edición más conocida del personaje.",
+        genre:"kodomo",
+        image:"https://m.media-amazon.com/images/I/51ca0oHIo4L._SX355_BO1,204,203,200_.jpg",
+    },
+    {
+        title:"Doraemon Color nº 03",
+        numVolume: 3,
+        collection:"Doraemon",
+        description:"Tomo manga kodomo de Doraemon, con elque se podrá disfrutar del personaje en su color original. Una colección que, además, recoge historias no publicadas en la edición más conocida del personaje.",
+        genre:"kodomo",
+        image:"https://m.media-amazon.com/images/I/61ZstE4E6sL._SX357_BO1,204,203,200_.jpg",
+    },
+    {
+        title:"Doraemon Color nº 04",
+        numVolume: 4,
+        collection:"Doraemon",
+        description:"Tomo manga kodomo de Doraemon, con elque se podrá disfrutar del personaje en su color original. Una colección que, además, recoge historias no publicadas en la edición más conocida del personaje.",
+        genre:"kodomo",
+        image:"https://m.media-amazon.com/images/I/61PzQyBNjkL._SX352_BO1,204,203,200_.jpg",
+    },
+    {
+        title:"Doraemon Color nº 05",
+        numVolume: 5,
+        collection:"Doraemon",
+        description:"Tomo manga kodomo de Doraemon, con elque se podrá disfrutar del personaje en su color original. Una colección que, además, recoge historias no publicadas en la edición más conocida del personaje.",
+        genre:"kodomo",
+        image:"https://m.media-amazon.com/images/I/51bUSG2OYeL._SX355_BO1,204,203,200_.jpg",
+    },
+    {
+        title:"Doraemon Color nº 06",
+        numVolume: 6,
+        collection:"Doraemon",
+        description:"Tomo manga kodomo de Doraemon, con elque se podrá disfrutar del personaje en su color original. Una colección que, además, recoge historias no publicadas en la edición más conocida del personaje.",
+        genre:"kodomo",
+        image:"https://m.media-amazon.com/images/I/51DlDOcMG4L._SX354_BO1,204,203,200_.jpg",
+    },
+    
+    
+]
---
+++ seed/shojo.js
@@ -0,0 +1,50 @@
+const ultraManiac=[
+    {
+        title:"Ultra Maniac. Los trucos de Nina",
+        numVolume: 1,
+        collection:"Ultra Maniac",
+        description:"Ayu Tateishi es la chica más popular del instituto. Su universo se centra en su imagen y en suspirar por el chico número uno, Tetsushi Kaji.La joven lleva una vida relativamente tranquila hasta que se cruza en su camino Nina Sakura. Ayu le ayuda a buscar un mini-ordenador, y la nueva estudiante, en agradecimiento, decide convertirse en su mejor amiga.El problema es que Nina es en realidad una bruja bastante inexperta, y la pobre Ayu va a sufrir las consecuencias de sus conjuros.",
+        author:"Wataru Yoshizumi",
+        genre:"Shojo",
+        image:"https://www.panini.es/media/catalog/product/cache/a87cfc69c89e7921deea3a8e066ac416/s/u/sultr001_0.jpg",
+    },
+    {
+        title:"Ultra Maniac. Los trucos de Nina",
+        numVolume:2 ,
+        collection:"Ultra Maniac",
+        description:"Ayu Tateishi es la chica más popular del instituto. Su universo se centra en su imagen y en suspirar por el chico número uno, Tetsushi Kaji.La joven lleva una vida relativamente tranquila hasta que se cruza en su camino Nina Sakura. Ayu le ayuda a buscar un mini-ordenador, y la nueva estudiante, en agradecimiento, decide convertirse en su mejor amiga.El problema es que Nina es en realidad una bruja bastante inexperta, y la pobre Ayu va a sufrir las consecuencias de sus conjuros.",
+        author:"Wataru Yoshizumi",
+        genre:"Shojo",
+        image:"https://www.panini.es/media/catalog/product/cache/a87cfc69c89e7921deea3a8e066ac416/s/u/sultr002_0.jpg",
+    },
+    {
+        title:"Ultra Maniac. Los trucos de Nina",
+        numVolume:3 ,
+        collection:"Ultra Maniac",
+        description:"Ayu Tateishi es la chica más popular del instituto. Su universo se centra en su imagen y en suspirar por el chico número uno, Tetsushi Kaji.La joven lleva una vida relativamente tranquila hasta que se cruza en su camino Nina Sakura. Ayu le ayuda a buscar un mini-ordenador, y la nueva estudiante, en agradecimiento, decide convertirse en su mejor amiga.El problema es que Nina es en realidad una bruja bastante inexperta, y la pobre Ayu va a sufrir las consecuencias de sus conjuros.",
+        author:"Wataru Yoshizumi",
+        genre:"Shojo",
+        image:"https://www.panini.es/media/catalog/product/cache/a87cfc69c89e7921deea3a8e066ac416/s/u/sultr003_0.jpg",
+    },
+    {
+        title:"Ultra Maniac. Los trucos de Nina",
+        numVolume:4 ,
+        collection:"Ultra Maniac",
+        description:"Ayu Tateishi es la chica más popular del instituto. Su universo se centra en su imagen y en suspirar por el chico número uno, Tetsushi Kaji.La joven lleva una vida relativamente tranquila hasta que se cruza en su camino Nina Sakura. Ayu le ayuda a buscar un mini-ordenador, y la nueva estudiante, en agradecimiento, decide convertirse en su mejor amiga.El problema es que Nina es en realidad una bruja bastante inexperta, y la pobre Ayu va a sufrir las consecuencias de sus conjuros.",
+        author:"Wataru Yoshizumi",
+        genre:"Shojo",
+        image:"https://www.panini.es/media/catalog/product/cache/a87cfc69c89e7921deea3a8e066ac416/s/u/sultr004_0.jpg",
+    },
+    {
+        title:"Ultra Maniac. Los trucos de Nina",
+        numVolume:5 ,
+        collection:"Ultra Maniac",
+        description:"Ayu Tateishi es la chica más popular del instituto. Su universo se centra en su imagen y en suspirar por el chico número uno, Tetsushi Kaji.La joven lleva una vida relativamente tranquila hasta que se cruza en su camino Nina Sakura. Ayu le ayuda a buscar un mini-ordenador, y la nueva estudiante, en agradecimiento, decide convertirse en su mejor amiga.El problema es que Nina es en realidad una bruja bastante inexperta, y la pobre Ayu va a sufrir las consecuencias de sus conjuros.",
+        author:"Wataru Yoshizumi",
+        genre:"Shojo",
+        image:"https://www.panini.es/media/catalog/product/cache/a87cfc69c89e7921deea3a8e066ac416/s/u/sultr005_0.jpg",
+    },
+    
+        
+    
+]
